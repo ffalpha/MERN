3f3bd7c1083cb859744af0bb0b388c9b56aedfc3
Kalana Mihiranga <kalanam217@gmail.com>
2021-02-08 11:39:45 +0530

    Added development scripts

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (res, req) => res.send("API RUNING"));
+app.get("/", (req, res) => res.send("API RUNING"));
 
 const PORT = process.env.PORT || 5000;
 
