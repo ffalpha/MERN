9456c5ef51732fbfb1e0944ca0cdf6cf2eca61e7
Kalana Mihiranga <kalanam217@gmail.com>
2021-02-08 13:29:47 +0530

    Added express validtor

--- Backend/routes/api/users.js
+++ Backend/routes/api/users.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 //@route post api/users
 //@desc Test route
 //@access PUblic
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("User route");
-});
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    //See if user exsits
+
+    //
+    res.send("User route");
+  }
+);
 
 module.exports = router;
