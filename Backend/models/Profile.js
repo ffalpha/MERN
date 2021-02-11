77db189bd6c0df658d91816e03d7fda4a6dc5fe1
Kalana Mihiranga <kalanam217@gmail.com>
2021-02-11 12:31:31 +0530

    Added Profile model

---
+++ Backend/models/Profile.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  company: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  Status: {
+    type: String,
+    required: true,
+  },
+  Skils: {
+    type: [String],
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+  githubusername: {
+    type: String,
+  },
+});
+
+module.exports = User = mongoose.model("users", UserSchema);
