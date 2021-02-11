08624581179c10ab01a366374cc7bddfa3a576a2
Kalana Mihiranga <kalanam217@gmail.com>
2021-02-12 00:49:20 +0530

    Fixed profile error

--- Backend/routes/api/profile.js
+++ Backend/routes/api/profile.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile");
 const User = require("../../models/Users");
+const { check, validationResult } = require("express-validator");
+
 //@route GET api/profile/me
 //@desc Get Current users profile
 //@access Private
@@ -14,10 +16,32 @@ router.get("/me", auth, async (req, res) => {
     if (!profile) {
       return res.status(400).json({ msg: "There is no profile for this user" });
     }
+    res.json(profile);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
   }
 });
+
+//@route Post api/profile/
+//@desc Get or Update User Profile
+//@access Private
+
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required").not().isEmpty(),
+      check("skills", "Skills is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ erros: errors.array() });
+    }
+  }
+);
 
 module.exports = router;
