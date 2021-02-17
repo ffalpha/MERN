6d2a839eac668b2938f3667a793784b7ce1fedf1
Kalana Mihiranga <kalanam217@gmail.com>
2021-02-17 19:29:31 +0530

    Get all profile and single profile done

--- Backend/routes/api/profile.js
+++ Backend/routes/api/profile.js
@@ -107,10 +107,32 @@ router.post(
 
 router.get("/", async (req, res) => {
   try {
-    const profile = await Profile.find().populate("user", ["name", "avatar"]);
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//@route Get api/profile/user/:user_id
+//@desc Get  profile by User ID
+//@access Public
+
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate("user", ["name", "avatar"]);
+    if (!profile) {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
     res.json(profile);
   } catch (error) {
     console.error(error.message);
+    if (error.kind == "ObjectId") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
     res.status(500).send("Server Error");
   }
 });
