34d56884d8288672913b9436b2b8258889ea467d
Kalana Mihiranga <kalanam217@gmail.com>
2021-05-06 08:38:02 +0530

    Add register forum

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Layout/Navbar';
+import Landing from './components/Layout/Landing';
+import Login from './components/auth/Login';
+import Regsiter from './components/auth/Register';
 const App = () => (
-  <Fragment>
-    <h1>App</h1>
-  </Fragment>
+  <Router>
+    {' '}
+    <Fragment>
+      <Navbar />
+
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Regsiter} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
